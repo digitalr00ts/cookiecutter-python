3b0e0352c1969b5cf7056e64354e0e0ab0b741eb
Carlos Meza <carlos@digitalr00ts.com>
2018-09-19 00:34:56 -0700

    add python module files

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_slug}}/exceptions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+{{cookiecutter.project_slug}}.exceptions
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+All the exceptions specific to this package.
+
+"""
+
+
+class GeneralException(Exception):
+    """Parent for all exceptions in this package"""
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def main():
+    """Command line entry point"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
