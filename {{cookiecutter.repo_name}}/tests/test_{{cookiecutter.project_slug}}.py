f8876603e777742ce091e36d32cb6afe268ec535
Carlos Meza <carlos@digitalr00ts.com>
2018-10-03 00:07:45 -0700

    enable logging in tests

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=no-self-use, invalid-name
 
-"""Tests for `{{ cookiecutter.project_slug }}` package."""
+"""Tests for {{ cookiecutter.project_slug }} package."""
 
+import logging
 import unittest
 
-import pytest
-
 import {{ cookiecutter.project_slug }}
+
+
+_logger: logging.Logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
 
 
 class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
