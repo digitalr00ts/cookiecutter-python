2b5fc74c496bc9fef595c83655c0895e763420df
Carlos Meza <carlos@digitalr00ts.com>
2019-01-26 20:50:26 -0800

    update setuptools

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -11,11 +11,11 @@ from setuptools import setup
 
 # Check for minimal version of setuptools
 SETUPTOOLS_VER = "30.5.0"
+
 try:
     require("setuptools>=" + SETUPTOOLS_VER)
 except VersionConflict:
     print("Error: version of setuptools is too old (<%s)!" % SETUPTOOLS_VER)
     sys.exit(1)
 
-if __name__ == "__main__":
-    setup()
+setup(use_scm_version=True)
