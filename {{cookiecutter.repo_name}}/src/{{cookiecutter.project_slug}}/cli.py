afaa298d8a478456a716271fec9db531cd129620
Carlos Meza <carlos@digitalr00ts.com>
2019-12-29 13:37:44 -0800

    refactor: change docopt to argparse

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/cli.py
@@ -1,34 +1,54 @@
 """{{ cookiecutter.project_name }}
 {{ cookiecutter.description }}
-
 Usage:
   {{ cookiecutter.project_slug }} (-h | --help)
   {{ cookiecutter.project_slug }} (-v | --version)
-
 Options:
   -v --version     Show version
   -h --help     Show this screen
 """
 
+import argparse
 import logging
+import sys
 from typing import Optional
 
-import pkg_resources
+from . import __version__
 
-from docopt import docopt
-
+_LOGGER = logging.getLogger(__name__)
 version: Optional[str] = None
 
-try:
-    version = pkg_resources.get_distribution(__package__).version
-except pkg_resources.DistributionNotFound:
-    version = None
+
+def _get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="{{ cookiecutter.project_name }} CLI Help",
+        allow_abbrev=False,
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
+    )
+
+    return parser
 
 
 def main():
-    arguments = docopt(__doc__, version=version, options_first=False)
-    logging.info("Arguments received: %s", arguments)
+    """ Main interface """
+    parser = _get_parser()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    cli_params = parser.parse_args()
+
+    from pprint import pprint
+    pprint(cli_params)
 
 
 if __name__ == "__main__":
     main()
+
