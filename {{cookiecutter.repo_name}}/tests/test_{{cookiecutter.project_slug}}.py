839c9fd313ffb481f3dd5cf333d0c7d0f0cd2f3a
Carlos Meza <carlos@digitalr00ts.com>
2020-07-28 02:04:05 -0700

    test: passing

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -14,11 +14,10 @@ import {{ cookiecutter.project_slug }}
 # better_exceptions.hook()
 
 
-class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
+class Test{{ cookiecutter.project_slug|title }}():
     """Tests for `{{ cookiecutter.project_slug }}` package."""
 
     def test_000_run(self):
       """Test run"""
       # Assert false because no tests have actually been implemented.
       assert False
-
