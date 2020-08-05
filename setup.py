6ec0b07d86ad9f4d82365f1d0ea971d289cad2e1
Carlos Meza <carlos@digitalr00ts.com>
2020-08-05 01:45:15 -0700

    chore(tests): cleanup setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""
+Shim to enable editable installs.
+Currently there is not a standard for editable installs. It is in discussion, 
+https://discuss.python.org/t/specification-of-editable-installation/1564/40.
+"""
+
+from setuptools import setup
+
+setup()
