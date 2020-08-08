5ff1dd42cf2908ea6f53c5f48049e427ed7c5d5c
Carlos Meza <carlos@digitalr00ts.com>
2020-08-08 00:53:40 -0700

    feat: switch to importlib instead of pkg_resources

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,10 +1,15 @@
 """ Package {{ cookiecutter.project_name }} top level """
+import importlib
 import logging
-import pkg_resources
+
+# importlib.metadata is implemented in Python 3.8
+# Previous versions require the backport, https://pypi.org/project/importlib-metadata/
+if not hasattr(importlib, "metadata"):
+    setattr(importlib, "metadata", importlib.import_module("importlib_metadata"))
 
 try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except pkg_resources.DistributionNotFound:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
     __version__ = None
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
