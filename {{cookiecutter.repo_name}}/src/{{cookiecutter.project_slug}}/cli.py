4323868d1ae86681379c38433f8581d0ab634070
Carlos Meza <carlos@digitalr00ts.com>
2020-08-12 00:11:16 -0700

    refactor(cli): cleanup

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/cli.py
@@ -13,42 +13,37 @@ import logging
 import sys
 from typing import Optional
 
-from . import __version__
+from {{ cookiecutter.project_slug }} import __version__
 
 _LOGGER = logging.getLogger(__name__)
 version: Optional[str] = None
 
 
-def _get_parser() -> argparse.ArgumentParser:
+def _create_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        description="{{ cookiecutter.project_name }} CLI Help",
-        allow_abbrev=False,
+        description="{{ cookiecutter.project_name }} CLI Help", allow_abbrev=False,
     )
 
     parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version=f"%(prog)s {__version__}",
+        "-v", "--version", action="version", version=f"%(prog)s {__version__}",
     )
 
     return parser
 
 
 def main():
-    """ Main interface """
-    parser = _get_parser()
+    """Console entrypoint"""
+    parser = _create_parser()
 
     if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
+        sys.exit(parser.print_help())
 
     cli_params = parser.parse_args()
 
-    from pprint import pprint
-    pprint(cli_params)
+    print("Parameters recieved:")
+    for param in cli_params:
+        print(param)
 
 
 if __name__ == "__main__":
     main()
-
