d10ed2674a7362e331dc8f8c52763e0684da521e
Carlos Meza <carlos@digitalr00ts.com>
2020-08-12 02:20:42 -0700

    fix(__main__): import cli

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/__main__.py
@@ -2,12 +2,14 @@
 import logging
 import sys
 
+from {{ cookiecutter.project_slug }}.cli import main as cli
+
 
 def main():
     """Module entry point"""
     module_args = sys.argv[1:]
     logging.debug("Module arguments received: %s", module_args)
-    {{ cookiecutter.project_slug }}.cli.main()
+    cli()
 
 
 if __name__ == "__main__":
