045737c52c30ea6a3973783df867deac499bd0d9
Carlos Meza <carlos@digitalr00ts.com>
2020-08-13 02:48:33 -0700

    style(tests): minor cleanup

--- {{cookiecutter.repo_name}}/tests/conftest.py
+++ {{cookiecutter.repo_name}}/tests/conftest.py
@@ -5,4 +5,3 @@ import {{ cookiecutter.project_slug }}
 def pytest_report_header():
     """ Additional report header """
     return f"version: { {{- cookiecutter.project_slug -}}.__version__}"
-
--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,12 +1,10 @@
 # pylint: disable=no-self-use, invalid-name, too-few-public-methods
 """Tests for {{ cookiecutter.project_slug }} package."""
 
-import pytest  # pytest: disable=unused-import
-
 import {{ cookiecutter.project_slug }}
 
 
-class Test{{ cookiecutter.project_slug|title }}():
+class Test{{ cookiecutter.project_slug|replace('_', ' ')|title|replace(' ', '') }}:
     """Tests for `{{ cookiecutter.project_slug }}` package."""
 
     def test_000_run(self):
