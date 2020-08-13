41c8e0455c577b675f4a8fbe7a96c4f20df77b10
Carlos Meza <carlos@digitalr00ts.com>
2020-08-12 21:40:30 -0700

    fix: importing metadata for importlib

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,15 +1,17 @@
 """{{ cookiecutter.project_name }} top level"""
-import importlib
 import logging
+from importlib import import_module
 
 # importlib.metadata is implemented in Python 3.8
 # Previous versions require the backport, https://pypi.org/project/importlib-metadata/
-if not hasattr(importlib, "metadata"):
-    setattr(importlib, "metadata", importlib.import_module("importlib_metadata"))
+try:
+    metadata = import_module("importlib.metadata")
+except ModuleNotFoundError:
+    metadata = import_module("importlib_metadata")
 
 try:
-    __version__ = importlib.metadata.version(__name__)
-except importlib.metadata.PackageNotFoundError:
+    __version__ = metadata.version(__name__)
+except metadata.PackageNotFoundError:
     __version__ = None
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
