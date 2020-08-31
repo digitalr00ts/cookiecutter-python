0311e0de0978662fa4f129a4598ffe6dfc2340ea
Carlos Meza <carlos@digitalr00ts.com>
2020-08-31 01:55:50 -0700

    feat(hooks): add git remote origin

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,4 +7,5 @@ git commit --allow-empty -m '🎉 initial commit'
 git add .
 git commit --message='✨ feat: initial project scaffolding'
 git tag {{ cookiecutter.version }}
+git remote add origin https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}.git
 ssh-keygen -t ed25519 -a 100 -N "" -C "{{ cookiecutter.project_slug }}@gh-actions" -f GH_DEPLOY_ed25519-key
